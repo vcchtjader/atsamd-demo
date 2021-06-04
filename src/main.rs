35bdfefeaaa7b98fae5ee6476c486237771a33f4
Henrik Tjäder <henrik.tjader@volvocars.com>
2021-06-04 19:21:43 +0200

    Get gclk_out to gclk_in working

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@
 use panic_halt as _;
 
 use atsamd_hal::{
-    clock::v2::{gclk, xosc::*, xosc32k::*, Dpll, GclkIn, GclkOut, Pclk, Tokens, GclkDiv},
+    clock::v2::{
+        gclk, xosc::*, xosc32k::*, Dpll, Gclk1Div, GclkDiv, GclkIn, GclkOut, Pclk, Tokens,
+    },
     gpio::v2::Pins,
     time::U32Ext,
 };
@@ -15,6 +17,9 @@ use rtic::app;
 
 #[app(device = atsamd_hal::target_device, peripherals = true )]
 mod app {
+
+    use atsamd_hal::clock::v2::DpllXoscPreDivider;
+    use cortex_m::interrupt::disable;
 
     use super::*;
 
@@ -38,39 +43,66 @@ mod app {
         let xosc0 =
             Xosc::from_crystal(tokens.sources.xosc0, pins.pa14, pins.pa15, 8.mhz()).enable();
 
-        // Configure DPLL0 to 100 MHz
-        let (dpll0, _xosc0) = Dpll::from_xosc(tokens.sources.dpll0, xosc0);
-        let dpll0 = dpll0.set_source_div(1).set_loop_div(50, 0).enable();
+        // Take DFLL 48 MHz, divide down to 2 MHz for Gclk1
+        let (gclk1, dfll) = gclk::Gclk::new(tokens.gclks.gclk1, dfll);
+        let gclk1 = gclk1.div(Gclk1Div::Div(24)).enable();
 
-        // Change Gclk0 from Dfll to Dpll0, MCLK = 100 MHz
+        // Enable output for 2 MHz clock on PB15
+        let (_gclk_out1, gclk1) =
+            GclkOut::enable(tokens.sources.gclk_io.gclk_out1, pins.pb15, gclk1, false);
+
+        // Either from pclk or xosc0
+        // =========================
+
+        // Setup DPLL0 using pclk of GCLK1
+        //let (pclk_dpll0, _gclk1) = Pclk::enable(tokens.pclks.dpll0, gclk1);
+        //let pclk_dpll0 = Dpll::from_pclk(tokens.sources.dpll0, pclk_dpll0);
+        //let dpll0 = pclk_dpll0.set_loop_div(50, 0).enable();
+
+        // Configure DPLL0 to 100 MHz fed from Xosc0
+        let (dpll0, _xosc0) =
+            Dpll::from_xosc(tokens.sources.dpll0, xosc0, DpllXoscPreDivider::Div(1));
+
+        // Use the predivider, 8 MHz / 4 * 50 = 100 MHz
+
+        let dpll0 = dpll0
+            .set_source_div(DpllXoscPreDivider::Div(1))
+            .set_loop_div(50, 0)
+            .enable();
+
+        // Disable the DPLL0
+        let dpll0 = dpll0.disable();
+
+        // Configure DPLL0 with 8 / 4 * 60 = 120 MHz
+        let dpll0 = dpll0
+            .set_source_div(DpllXoscPreDivider::Div(1))
+            .set_loop_div(60, 0)
+            .enable();
+
+        //// Change Gclk0 from Dfll to Dpll0, MCLK = 100 MHz
         let (gclk0, _dfll, _dpll0) = gclk0.swap(dfll, dpll0);
 
-        // Output Gclk0 on pin PB14 (demo optional)
+        //// Output Gclk0 on pin PB14 (demo optional)
         let (_gclk_out0, _gclk0) =
             GclkOut::enable(tokens.sources.gclk_io.gclk_out0, pins.pb14, gclk0, false);
 
-        // ----
-        // Input for Gclk6 on pin PB20 (assumed frequency of 10 Mhz)
-        let gclk_in6 = GclkIn::enable(tokens.sources.gclk_io.gclk_in6, pins.pb20, 2.mhz());
+        //// ----
+        //// Input for Gclk3 on pin PB17 (assumed frequency of 2 MHz)
+        let gclk_in3 = GclkIn::enable(tokens.sources.gclk_io.gclk_in3, pins.pb17, 2.mhz());
+        let (gclk3, _gclk_in3) = gclk::Gclk::new(tokens.gclks.gclk3, gclk_in3);
+        let gclk3 = gclk3.enable();
 
-        let (gclk6, _gclk_in6) = gclk::Gclk::new(tokens.gclks.gclk6, gclk_in6);
-        let gclk6 = gclk6.enable();
-
-        let (pclk_dpll1, _gclk6) = Pclk::enable(tokens.pclks.dpll1, gclk6);
-
-        // Configure DPLL1 to 200 MHz
+        // Setup DPLL1 with input from Gclk3, fed from external 2 MHz signal on pin PB17
+        let (pclk_dpll1, _gclk3) = Pclk::enable(tokens.pclks.dpll1, gclk3);
         let dpll1 = Dpll::from_pclk(tokens.sources.dpll1, pclk_dpll1);
-        let dpll1 = dpll1.set_source_div(1).set_loop_div(200, 0).enable();
+        // Configure DPLL1 to run at 2 * 50 = 100 MHz
+        let dpll1 = dpll1.set_loop_div(50, 0).enable();
 
-        let (gclk1, dpll1) = gclk::Gclk::new(tokens.gclks.gclk1, dpll1);
-        let gclk1 = gclk1.enable();
-        let (_gclk_out1, _gclk1) =
-            GclkOut::enable(tokens.sources.gclk_io.gclk_out1, pins.pb15, gclk1, false);
-
-        let (gclk3, _dpll1) = gclk::Gclk::new(tokens.gclks.gclk3, dpll1);
-        let gclk3 = gclk3.div(GclkDiv::Div(10)).enable();
-        let (_gclk_out3, _gclk3) =
-            GclkOut::enable(tokens.sources.gclk_io.gclk_out3, pins.pa17, gclk3, false);
+        // Output DPLL1 on PB20 via Gclk6, divided by 200 resulting in 0.5 MHz output
+        let (gclk6, _dpll1) = gclk::Gclk::new(tokens.gclks.gclk6, dpll1);
+        let gclk6 = gclk6.div(GclkDiv::Div(200)).enable();
+        let (_gclk_out6, _gclk6) =
+            GclkOut::enable(tokens.sources.gclk_io.gclk_out6, pins.pb20, gclk6, false);
 
         // ----
         // Enable external 32k-oscillator
@@ -78,8 +110,20 @@ mod app {
             .enable_32k(true)
             .enable();
 
+        // Xosc32k = 32kHz Expressed as MHz: >>> 32*1024/1000/1000 = 0.032768
+        // 100 / 0.032768 = 3051.7578125
+        //
+        // Missing 0.7578...
+        //
+        // Use fractional divider: x / 32 = 0.75..
+        // 24/32 = 0.75
+        //
+        // 3052 * (32*1024/1000/1000) = 100.007936
+        //let (dpll1, xosc32k) = Dpll::from_xosc32k(tokens.sources.dpll1, xosc32k);
+        //let _dpll1 = dpll1.set_loop_div(3000, 24).enable();
+
         let (gclk2, _xosc32k) = gclk::Gclk::new(tokens.gclks.gclk2, xosc32k);
-        let gclk2 = gclk2.div(gclk::GclkDiv::Div2Pow8).enable();
+        let gclk2 = gclk2.div(gclk::GclkDiv::Div(2)).enable();
         let (_gclk_out2, _gclk2) =
             GclkOut::enable(tokens.sources.gclk_io.gclk_out2, pins.pb16, gclk2, false);
 
