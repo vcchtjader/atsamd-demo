87b6b92418e8f810f29c2d40d358379901ec4de4
Henrik Tjäder <henrik.tjader@volvocars.com>
2021-06-10 07:45:57 +0200

    Update to use latest clocking-api-v2

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use panic_halt as _;
 
 use atsamd_hal::{
     clock::v2::{
-        gclk, xosc::*, xosc32k::*, Dpll, Gclk1Div, GclkDiv, GclkIn, GclkOut, Pclk, Tokens,
+        gclk, retrieve_clocks, xosc::*, xosc32k::*, Dpll, Gclk1Div, GclkDiv, GclkIn, GclkOut, Pclk,
     },
     gpio::v2::Pins,
     time::U32Ext,
@@ -18,8 +18,7 @@ use rtic::app;
 #[app(device = atsamd_hal::target_device, peripherals = true )]
 mod app {
 
-    use atsamd_hal::clock::v2::DpllXoscPreDivider;
-    use cortex_m::interrupt::disable;
+    //use cortex_m::interrupt::disable;
 
     use super::*;
 
@@ -28,7 +27,7 @@ mod app {
         let mut device = cx.device;
 
         // Get the clocks & tokens
-        let (gclk0, dfll, osculp32k, tokens) = Tokens::new(
+        let (gclk0, dfll, osculp32k, tokens) = retrieve_clocks(
             device.OSCCTRL,
             device.OSC32KCTRL,
             device.GCLK,
@@ -40,61 +39,53 @@ mod app {
         let pins = Pins::new(device.PORT);
 
         // Enable pin PA14 and PA15 as an external source for XOSC0 at 8 MHz
-        let xosc0 =
-            Xosc::from_crystal(tokens.sources.xosc0, pins.pa14, pins.pa15, 8.mhz()).enable();
+        let xosc0 = Xosc::from_crystal(tokens.xosc0, pins.pa14, pins.pa15, 8.mhz()).enable();
 
         // Take DFLL 48 MHz, divide down to 2 MHz for Gclk1
         let (gclk1, dfll) = gclk::Gclk::new(tokens.gclks.gclk1, dfll);
         let gclk1 = gclk1.div(Gclk1Div::Div(24)).enable();
 
         // Enable output for 2 MHz clock on PB15
-        let (_gclk_out1, gclk1) =
-            GclkOut::enable(tokens.sources.gclk_io.gclk_out1, pins.pb15, gclk1, false);
+        let (_gclk_out1, _gclk1) =
+            GclkOut::enable(tokens.gclk_io.gclk_out1, pins.pb15, gclk1, false);
 
         // Either from pclk or xosc0
         // =========================
 
         // Setup DPLL0 using pclk of GCLK1
         //let (pclk_dpll0, _gclk1) = Pclk::enable(tokens.pclks.dpll0, gclk1);
-        //let pclk_dpll0 = Dpll::from_pclk(tokens.sources.dpll0, pclk_dpll0);
+        //let pclk_dpll0 = Dpll::from_pclk(tokens.dpll0, pclk_dpll0);
         //let dpll0 = pclk_dpll0.set_loop_div(50, 0).enable();
 
         // Configure DPLL0 to 100 MHz fed from Xosc0
-        let (dpll0, _xosc0) =
-            Dpll::from_xosc(tokens.sources.dpll0, xosc0, DpllXoscPreDivider::Div(1));
+        let (dpll0, _xosc0) = Dpll::from_xosc(tokens.dpll0, xosc0, 1);
 
         // Use the predivider, 8 MHz / 4 * 50 = 100 MHz
 
-        let dpll0 = dpll0
-            .set_source_div(DpllXoscPreDivider::Div(1))
-            .set_loop_div(50, 0)
-            .enable();
+        let dpll0 = dpll0.set_source_div(1).set_loop_div(50, 0).enable();
 
         // Disable the DPLL0
         let dpll0 = dpll0.disable();
 
         // Configure DPLL0 with 8 / 4 * 60 = 120 MHz
-        let dpll0 = dpll0
-            .set_source_div(DpllXoscPreDivider::Div(1))
-            .set_loop_div(60, 0)
-            .enable();
+        let dpll0 = dpll0.set_source_div(1).set_loop_div(60, 0).enable();
 
         //// Change Gclk0 from Dfll to Dpll0, MCLK = 100 MHz
         let (gclk0, _dfll, _dpll0) = gclk0.swap(dfll, dpll0);
 
-        //// Output Gclk0 on pin PB14 (demo optional)
+        //// Output Gclk0 on pin PB14
         let (_gclk_out0, _gclk0) =
-            GclkOut::enable(tokens.sources.gclk_io.gclk_out0, pins.pb14, gclk0, false);
+            GclkOut::enable(tokens.gclk_io.gclk_out0, pins.pb14, gclk0, false);
 
         //// ----
         //// Input for Gclk3 on pin PB17 (assumed frequency of 2 MHz)
-        let gclk_in3 = GclkIn::enable(tokens.sources.gclk_io.gclk_in3, pins.pb17, 2.mhz());
+        let gclk_in3 = GclkIn::enable(tokens.gclk_io.gclk_in3, pins.pb17, 2.mhz());
         let (gclk3, _gclk_in3) = gclk::Gclk::new(tokens.gclks.gclk3, gclk_in3);
         let gclk3 = gclk3.enable();
 
         // Setup DPLL1 with input from Gclk3, fed from external 2 MHz signal on pin PB17
         let (pclk_dpll1, _gclk3) = Pclk::enable(tokens.pclks.dpll1, gclk3);
-        let dpll1 = Dpll::from_pclk(tokens.sources.dpll1, pclk_dpll1);
+        let dpll1 = Dpll::from_pclk(tokens.dpll1, pclk_dpll1);
         // Configure DPLL1 to run at 2 * 50 = 100 MHz
         let dpll1 = dpll1.set_loop_div(50, 0).enable();
 
@@ -102,11 +93,11 @@ mod app {
         let (gclk6, _dpll1) = gclk::Gclk::new(tokens.gclks.gclk6, dpll1);
         let gclk6 = gclk6.div(GclkDiv::Div(200)).enable();
         let (_gclk_out6, _gclk6) =
-            GclkOut::enable(tokens.sources.gclk_io.gclk_out6, pins.pb20, gclk6, false);
+            GclkOut::enable(tokens.gclk_io.gclk_out6, pins.pb20, gclk6, false);
 
         // ----
         // Enable external 32k-oscillator
-        let xosc32k = Xosc32k::from_crystal(tokens.sources.xosc32k, pins.pa00, pins.pa01)
+        let xosc32k = Xosc32k::from_crystal(tokens.xosc32k, pins.pa00, pins.pa01)
             .enable_32k(true)
             .enable();
 
@@ -119,18 +110,18 @@ mod app {
         // 24/32 = 0.75
         //
         // 3052 * (32*1024/1000/1000) = 100.007936
-        //let (dpll1, xosc32k) = Dpll::from_xosc32k(tokens.sources.dpll1, xosc32k);
+        //let (dpll1, xosc32k) = Dpll::from_xosc32k(tokens.dpll1, xosc32k);
         //let _dpll1 = dpll1.set_loop_div(3000, 24).enable();
 
         let (gclk2, _xosc32k) = gclk::Gclk::new(tokens.gclks.gclk2, xosc32k);
         let gclk2 = gclk2.div(gclk::GclkDiv::Div(2)).enable();
         let (_gclk_out2, _gclk2) =
-            GclkOut::enable(tokens.sources.gclk_io.gclk_out2, pins.pb16, gclk2, false);
+            GclkOut::enable(tokens.gclk_io.gclk_out2, pins.pb16, gclk2, false);
 
         let (gclk5, _osculp32k) = gclk::Gclk::new(tokens.gclks.gclk5, osculp32k);
         let gclk5 = gclk5.div(gclk::GclkDiv::Div(0)).enable();
         let (_gclk_out5, _gclk5) =
-            GclkOut::enable(tokens.sources.gclk_io.gclk_out5, pins.pb11, gclk5, false);
+            GclkOut::enable(tokens.gclk_io.gclk_out5, pins.pb11, gclk5, false);
 
         (init::LateResources {}, init::Monotonics())
     }
